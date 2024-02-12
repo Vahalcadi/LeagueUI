ce45bd87345bf078fd737ff325cd5de00ded6965
Vahalcadi <davidepetrone04@gmail.com>
2024-02-12 19:17:37 +0100

    adjusted Summoner creation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -17,11 +18,9 @@ public class GameManager : MonoBehaviour
     private TextMeshProUGUI minionCounter;
     private TextMeshProUGUI kda;
 
-
     // Start is called before the first frame update
     void Start()
     {
-        InstantiateSummoners(10);
     }
 
     // Update is called once per frame
@@ -31,23 +30,35 @@ public class GameManager : MonoBehaviour
     }
 
 
-    public void InstantiateSummoners(int numberOfSummoners)
+    public void InstantiateSummoners(string numberOfSummoners)
     {
-        if(numberOfSummoners > 10)
-            numberOfSummoners = 10;
+        if (summonerSlotList.Count > 0)
+        {
+            foreach (var item in summonerSlotList)
+            {
+                Destroy(item.gameObject);
+            }
 
-        for (int i = 0; i < numberOfSummoners; i++)
+            summonerSlotList.Clear();
+        }
+
+        int summoners = Convert.ToInt32(numberOfSummoners);
+
+        if(summoners > 10)
+            summoners = 10;
+
+        for (int i = 0; i < summoners; i++)
         {
             summonerSlotList.Add(Instantiate(summonerSlot, scoreboard.transform));
 
-            summonerSlotList[i].transform.Find("SummonerSpell1").transform.Find("Image").GetComponent<Image>().sprite = summonerSpells[Random.Range(0, summonerSpells.Count)];
-            summonerSlotList[i].transform.Find("SummonerSpell2").transform.Find("Image").GetComponent<Image>().sprite = summonerSpells[Random.Range(0, summonerSpells.Count)];
-            summonerSlotList[i].transform.Find("CharacterSlot").transform.Find("CharacterIcon").transform.Find("Image").GetComponentInChildren<Image>().sprite = championIcon[Random.Range(0, championIcon.Count)];
+            summonerSlotList[i].transform.Find("SummonerSpell1").transform.Find("Image").GetComponent<Image>().sprite = summonerSpells[UnityEngine.Random.Range(0, summonerSpells.Count)];
+            summonerSlotList[i].transform.Find("SummonerSpell2").transform.Find("Image").GetComponent<Image>().sprite = summonerSpells[UnityEngine.Random.Range(0, summonerSpells.Count)];
+            summonerSlotList[i].transform.Find("CharacterSlot").transform.Find("CharacterIcon").transform.Find("Image").GetComponentInChildren<Image>().sprite = championIcon[UnityEngine.Random.Range(0, championIcon.Count)];
             summonerSlotList[i].transform.Find("UltimateSlot").transform.Find("RechargeUltimate").GetComponent<Image>().fillAmount = 0;
             summonerSlotList[i].transform.Find("UltimateSlot").transform.Find("FullUltimate").GetComponent<Image>().color = Color.clear;
             summonerSlotList[i].transform.Find("LevelSlot").GetComponentInChildren<TextMeshProUGUI>().text = "1";
-            summonerSlotList[i].transform.Find("PrimaryRune").GetComponent<Image>().sprite = keyRunes[Random.Range(0, keyRunes.Count)];
-            summonerSlotList[i].transform.Find("SecondaryRune").GetComponent<Image>().sprite = secondaryRunes[Random.Range(0, secondaryRunes.Count)];
+            summonerSlotList[i].transform.Find("PrimaryRune").GetComponent<Image>().sprite = keyRunes[UnityEngine.Random.Range(0, keyRunes.Count)];
+            summonerSlotList[i].transform.Find("SecondaryRune").GetComponent<Image>().sprite = secondaryRunes[UnityEngine.Random.Range(0, secondaryRunes.Count)];
             summonerSlotList[i].transform.Find("MinionCounter").GetComponent<TextMeshProUGUI>().text = "0";
             summonerSlotList[i].transform.Find("KDA").GetComponent<TextMeshProUGUI>().text = "0/0/0";
 
